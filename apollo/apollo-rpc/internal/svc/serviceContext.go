5653d52bba1fd23ce6821b1a5b03f3e7ae9617a3
Jian Qi <e.jianqi@gmail.com>
2025-07-31 07:11:17 +0800

    Feature: Login/Registration via Email & Password

--- apollo/apollo-rpc/internal/svc/serviceContext.go
+++ apollo/apollo-rpc/internal/svc/serviceContext.go
@@ -1,46 +1,25 @@
 package svc
 
 import (
-	"github.com/go-webauthn/webauthn/webauthn"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
-
 	"jian-unified-system/apollo/apollo-rpc/internal/config"
 	"jian-unified-system/apollo/apollo-rpc/internal/model"
+	"jian-unified-system/jus-core/util"
 )
 
 type ServiceContext struct {
-	Config   config.Config
-	WebAuthn *webauthn.WebAuthn // 新增成员
+	Config config.Config
 
-	UserModel          model.UserModel
-	PasskeysModel      model.PasskeysModel
-	PasskeyModel       model.PasskeyModel
-	AuthenticatorModel model.AuthenticatorModel
+	UserModel model.UserModel
+
+	MLKEMKeyManager util.MLKEMKeyManager
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	// 初始化WebAuthn
-	wa, err := webauthn.New(&webauthn.Config{
-		RPID:          c.WebAuthn.RPID,
-		RPDisplayName: c.WebAuthn.RPDisplayName,
-		RPOrigins:     c.WebAuthn.RPOrigins,
-		// ⚠️ 启用 discoverable login
-		//AuthenticatorSelection: protocol.AuthenticatorSelection{
-		//	ResidentKey:      protocol.ResidentKeyRequirementRequired,
-		//	UserVerification: protocol.VerificationRequired,
-		//},
-	})
-	if err != nil {
-		panic("初始化WebAuthn失败: " + err.Error())
-	}
-
 	sqlConn := sqlx.NewMysql(c.DB.DataSource)
 	return &ServiceContext{
-		Config:             c,
-		WebAuthn:           wa,
-		UserModel:          model.NewUserModel(sqlConn, c.Cache),
-		PasskeysModel:      model.NewPasskeysModel(sqlConn, c.Cache),
-		PasskeyModel:       model.NewPasskeyModel(sqlConn, c.Cache),
-		AuthenticatorModel: model.NewAuthenticatorModel(sqlConn, c.Cache),
+		Config:          c,
+		UserModel:       model.NewUserModel(sqlConn, c.Cache),
+		MLKEMKeyManager: util.DefaultMLKEMKeyManager(),
 	}
 }
