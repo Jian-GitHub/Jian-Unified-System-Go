50e97d3548d6cde832b6b9a5f34e83e282a1a459
Jian Qi <e.jianqi@gmail.com>
2025-08-03 00:43:08 +0800

    Fix Bug

--- apollo/apollo-api/internal/handler/routes.go
+++ apollo/apollo-api/internal/handler/routes.go
@@ -72,7 +72,7 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 	server.AddRoutes(
 		[]rest.Route{
 			{
-				Method:  http.MethodPost,
+				Method:  http.MethodGet,
 				Path:    "/Callback/:provider",
 				Handler: thirdParty.CallbackHandler(serverCtx),
 			},
