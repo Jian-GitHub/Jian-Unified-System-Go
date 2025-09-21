fa0cabfc1ed3da44d2ca38a91ba3ea619c786e2e
Jian Qi <e.jianqi@gmail.com>
2025-09-21 15:52:20 +0800

    Remove Kafka

--- jquantum/jquantum-rpc/jquantumrpc.go
+++ jquantum/jquantum-rpc/jquantumrpc.go
@@ -3,18 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"jian-unified-system/jquantum/jquantum-rpc/internal/config"
 	jquantumServer "jian-unified-system/jquantum/jquantum-rpc/internal/server/jquantum"
 	jobService "jian-unified-system/jquantum/jquantum-rpc/internal/service/job"
 	"jian-unified-system/jquantum/jquantum-rpc/internal/svc"
 	"jian-unified-system/jquantum/jquantum-rpc/jquantum"
 	"jian-unified-system/jus-hermes/mq/rabbitMQ"
-
-	"github.com/zeromicro/go-zero/core/conf"
-	"github.com/zeromicro/go-zero/core/service"
-	"github.com/zeromicro/go-zero/zrpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 var configFile = flag.String("f", "etc/jquantumrpc.yaml", "the config file")
@@ -38,7 +37,7 @@ func main() {
 	consumer := rabbitMQ.NewConsumer(c.RabbitMQ, ctx.Redis, jobService.NewExecutor(ctx).Process)
 	// 启动消费者
 	consumer.StartConsuming()
-	go ctx.StartKafkaConsumer()
+	//go ctx.StartKafkaConsumer()
 
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
 	s.Start()
